d73ac1008fab0fee378aa05e53de818b04374873
dasha-k <dashakondrattenko@gmail.com>
2020-08-05 12:22:15 -0700

    Crate folders structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
-import React from "react"
+import React from "react";
+
+import Layout from "../components/layout"; 
 
 export default function Home() {
-  return <div><h1>Hello test</h1></div>
+	return (
+		<Layout>
+			<div><h1>Hello test</h1></div>
+		</Layout>
+	); 
 }
 
 
